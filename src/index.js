5673d1647244719f0351529ae45e8cf062b9d022
Prawira <prawira.96@gmail.com>
2019-03-14 21:11:07 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
+//components
+import Home from './Home';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const hist = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={hist}>
+    <Switch>
+      <Route path="/home" component={Home} />
+      <Redirect from="/" to="/home" />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);  
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+
+
+
+
+
