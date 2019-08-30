3c74541279e2159334ae526dd6fa8592ea29687a
PrawiraGenestonlia <prawira.96@gmail.com>
2019-08-30 16:46:07 +0800

    v2

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -40,18 +40,41 @@ class Header extends Component {
           </nav>
 
           <div className="row banner" style={{ width: '100%', textAlign: 'center', position: 'relative', zIndex: 4 }}>
-            <div className="banner-text">
-              <center>
-                <h1 className="responsive-headline">I'm {name}.</h1>
-                <h3>{homepageDescriptionPart1} <span>{homepageDescriptionPart2}</span>. {description}.</h3>
+
+            <div className="title">
+              <svg className="title" height="100%" width='100%' viewbox="0 0 400 400" style={{ textAlign: 'center' }}>
+                <defs>
+                  <linearGradient id="gradient" x1="0" x2="0" y1="0" y2="1">
+                    <stop offset="5%" stop-color="#326384" />
+                    <stop offset="95%" stop-color="#123752" />
+                  </linearGradient>
+                  <pattern id="wave" x="0" y="0" width="120" height="110" patternUnits="userSpaceOnUse">
+                    <path id="wavePath" d="M-40 9 Q-30 7 -20 9 T0 9 T20 9 T40 9 T60 9 T80 9 T100 9 T120 9 V20 H-40z" mask="url(#mask)" fill="url(#gradient)">
+                      <animateTransform
+                        attributeName="transform"
+                        begin="0s"
+                        dur="1.5s"
+                        type="translate"
+                        from="0,0"
+                        to="40,0"
+                        repeatCount="indefinite" />
+                    </path>
+                  </pattern>
+                </defs>
+                <text text-anchor="middle" x="200" y="130" font-size="6rem" fill="url(#wave)" fill-opacity="0.5">I'm Prawira.</text>
+                <text text-anchor="middle" x="200" y="130" font-size="6rem" fill="url(#gradient)" fill-opacity="0.3">I'm Prawira.</text>
+              </svg>
+            </div>
+            <center>
+              <div className="banner-text" style={{ backgroundColor: "rgba(0,0,0,0.8)", borderRadius: '25px', width: '90%', padding: "1%" }}>
+                <h3><b>{homepageDescriptionPart1} <span>{homepageDescriptionPart2}</span>. {description}.</b></h3>
 
 
                 <ul className="social">
                   {networks}
                 </ul>
-
-              </center>
-            </div>
+              </div>
+            </center>
           </div>
           <p className="scrolldown">
             <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
